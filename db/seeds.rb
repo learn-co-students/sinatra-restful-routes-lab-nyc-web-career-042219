1743ef57f26becef2b575cd291b9a716751a6bb2
Carly Pecora <carly@carlypecora.com>
2019-05-14 19:52:47 -0400

    Done.

---
+++ db/seeds.rb
@@ -0,0 +1,3 @@
+cookies = Recipe.create!({name: "chocolate", ingredients: "sugar", cook_time: "20 mins"})
+cake = Recipe.create!({name: "vanilla", ingredients: "flour", cook_time: "30 mins"})
+bread = Recipe.create!({name: "wheat", ingredients: "butter and flour", cook_time: "1 hour"})
